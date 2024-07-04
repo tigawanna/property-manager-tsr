f81e3ee8f9fc145ece4151955d9b6424e53a348f
tigawanna <denniskinuthiawaweru@gmail.com>
2024-07-04 16:39:26 +0300

    fix up toolbar

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -3,21 +3,37 @@ import Nprogress from "./navigation/nprogress/Nprogress";
 import { MdCastle } from "react-icons/md";
 import { FaCircleUser } from "react-icons/fa6";
 import { MainSideDrawer } from "./navigation/MainSideDrawer";
+import { navbarRoutes } from "./navigation/navbar-routes";
+import { ThemeToggle } from "./navigation/ThemeToggle";
 interface MainNavbarProps {}
 
 export function MainNavbar({}: MainNavbarProps) {
   const isLoading = useRouterState({ select: (s) => s.status === "pending" });
   return (
-    <nav className="w-full flex flex-col items-center justify-center py-1 px-3 border-b sticky top-0 z-30">
-      <div className="w-full h-full flex items-center justify-between">
+    <nav className="w-full h-full flex flex-co  items-center justify-center py-1 px-3 border-b sticky top-0 z-30">
+      <div className="w-full h-full flex items-center justify-between ">
         <Link to="/" className="">
           <MdCastle className="size-8" />
         </Link>
-        <Link to="/admin" className="">admin</Link>
-        <Link to="/auth">
-          <FaCircleUser className="size-6"/>
-        </Link>
-        <MainSideDrawer/>
+        <div className=" h-full hidden md:flex  gap-3 px-6 py-1 justify-center items-center rounded-full border">
+          {navbarRoutes.map((route) => {
+            if (route.path === "/") return;
+            return (
+              <Link key={route.path} to={route.path} className="h-full  hover:text-accent-text">
+                {route.name}
+              </Link>
+            );
+          })}
+        </div>
+        <div className=" h-full hidden sm:flex gap-2 justify-center items-center rounded-full ">
+          <Link to="/auth">
+            <FaCircleUser className="size-6" />
+          </Link>
+          <ThemeToggle />
+        </div>
+        <div className="h-full md:hidden">
+          <MainSideDrawer />
+        </div>
       </div>
       <Nprogress isAnimating={isLoading} />
     </nav>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ function IndexComponent() {
   return (
     <div className="min-h-screen  flex flex-col items-center justify-center h-2">
       <div className="bg-accent-default p-[5%]">
-        <h1 className="text-3xl text-accent-text">Accent test</h1>
+        <h1 className="text-3xl ">Accent test</h1>
       </div>
     </div>
   );
