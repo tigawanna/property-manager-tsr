4d8cef147e8c9f41aa461043dd37c439b09fe79f
tigawanna <denniskinuthiaw@gmail.com>
2024-07-06 20:22:39 +0300

    saved signuo.tsx

--- src/routes/auth/signup.tsx
+++ src/routes/auth/signup.tsx
@@ -1,5 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SignupComponent } from './-components/SignupComponent';
+import { z } from 'zod';
 
-export const Route = createFileRoute('/auth/signup')({
-  component: () => <div>Hello /auth/signup!</div>
-})
+const searchparams = z.object({
+  returnTo: z.string()
+});
+export const Route = createFileRoute("/auth/signup")({
+  component: SignupPage,
+  validateSearch: (search) => searchparams.parse(search),
+});
+
+interface SignupProps {
+
+}
+
+export function SignupPage({}:SignupProps){
+return (
+ <div className='w-full h-full min-h-screen flex flex-col items-center justify-center'>
+    <SignupComponent/>
+ </div>
+);
+}
