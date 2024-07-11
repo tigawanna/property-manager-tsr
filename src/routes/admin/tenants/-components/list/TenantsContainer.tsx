05c46d966f741f6ec62f6b066bfea303e7ad1074
tigawanna <denniskinuthiawaweru@gmail.com>
2024-07-11 11:41:33 +0300

    add shops list

---
+++ src/routes/admin/tenants/-components/list/TenantsContainer.tsx
@@ -0,0 +1,11 @@
+interface TenantsContainerProps {
+
+}
+
+export function TenantsContainer({}:TenantsContainerProps){
+return (
+ <div className='w-full h-full flex flex-col items-center justify-center'>
+
+ </div>
+);
+}
---
+++ src/routes/admin/tenants/-components/list/TenantsList.tsx
@@ -0,0 +1,11 @@
+interface TenantsListProps {
+
+}
+
+export function TenantsList({}:TenantsListProps){
+return (
+ <div className='w-full h-full flex flex-col items-center justify-center'>
+
+ </div>
+);
+}
